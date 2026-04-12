380575486df16b177dcae3a7a68870bacf757fa3
Mandavi <mandavikumari185@gmail.com>
2026-04-12 10:20:00 +0530

    Add countDigits method to Solution class

---
+++ day9.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countDigits(self, num):
+        count = 0
+        
+        for digit in str(num):
+            d = int(digit)
+            
+            if d != 0 and num % d == 0:
+                count += 1 
+                
+        return count 
+        
+       
