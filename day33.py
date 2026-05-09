ae0bd2290932323f32e1dcf3ca90cc91aad7ba86
Mandavi <mandavikumari185@gmail.com>
2026-05-09 23:43:03 +0530

    Implement grid rotation in Solution class

---
+++ day33.py
@@ -0,0 +1,61 @@
+class Solution:
+    def rotateGrid(self, grid, k):
+        m, n = len(grid), len(grid[0])
+
+        layers = min(m, n) // 2
+
+        for layer in range(layers):
+
+            # boundaries
+            top = layer
+            left = layer
+            bottom = m - layer - 1
+            right = n - layer - 1
+
+            arr = []
+
+            # top row
+            for j in range(left, right + 1):
+                arr.append(grid[top][j])
+
+            # right column
+            for i in range(top + 1, bottom):
+                arr.append(grid[i][right])
+
+            # bottom row
+            for j in range(right, left - 1, -1):
+                arr.append(grid[bottom][j])
+
+            # left column
+            for i in range(bottom - 1, top, -1):
+                arr.append(grid[i][left])
+
+            length = len(arr)
+
+            # counter-clockwise rotation
+            rot = k % length
+            rotated = arr[rot:] + arr[:rot]
+
+            idx = 0
+
+            # put back top row
+            for j in range(left, right + 1):
+                grid[top][j] = rotated[idx]
+                idx += 1
+
+            # put back right column
+            for i in range(top + 1, bottom):
+                grid[i][right] = rotated[idx]
+                idx += 1
+
+            # put back bottom row
+            for j in range(right, left - 1, -1):
+                grid[bottom][j] = rotated[idx]
+                idx += 1
+
+            # put back left column
+            for i in range(bottom - 1, top, -1):
+                grid[i][left] = rotated[idx]
+                idx += 1
+
+        return grid
